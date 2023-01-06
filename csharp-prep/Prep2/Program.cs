14e54d5ebfe2e97338f575ee6509bf8f9d943351
VladKaralius <vladislovas.karalius@gmail.com>
2023-01-06 15:27:00 +0200

    Prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string gradeLetter = "";
+        string message = "";
+        
+        
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+
+        if (grade >= 70)
+        {
+            message = "Congratulations. You have passed the course!";
+        }
+        else
+        {
+            message = "You failed the course. Better luck next time!";
+        }
+
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+        Console.WriteLine(message);
     }
 }
