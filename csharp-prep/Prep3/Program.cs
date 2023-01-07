3681a8a56da39a326c73c2d1b09776b15de7e47c
VladKaralius <vladislovas.karalius@gmail.com>
2023-01-07 12:48:08 +0200

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+
+        while (magic != guess)
+
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+
+        }
+
     }
 }
