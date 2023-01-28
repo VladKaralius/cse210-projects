e259bf75a243ab0cf9a57b39b8cc65710319137e
VladKaralius <vladislovas.karalius@gmail.com>
2023-01-28 22:11:23 +0200

    Tuple test

--- prove/Develop03/ReferenceGenerator.cs
+++ prove/Develop03/ReferenceGenerator.cs
@@ -16,14 +16,24 @@
             _masteryList = System.IO.File.ReadAllLines(_fileName);
         }
 
-        public string[] GetReference()
+        public ValueTuple<string, int, int> GetReference()
         {
             // Choose a random line
             Random random = new Random();
             int index = random.Next(_masteryList.Length);
             string line = _masteryList[index];
             // Split the line
-            string[] reference = line.Split("|");
+            string[] parts = line.Split("|");
+
+            string book = parts[0];
+            int chapter = int.Parse(parts[1]);
+            int verse = int.Parse(parts[2]);
+
+            if (parts.Length == 3)
+            {
+                var reference = ValueTuple.Create(book, chapter, verse);
+                return reference;
+            }
             
             return reference;
         }
