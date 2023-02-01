60194897523358f879194ce7f3c621ce80a4e710
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-01 16:09:25 +0200

    Learning04 tests

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+
+    Student student = new Student("Brigham", "234");
+    string name = student.GetName();
+    string number = student.GetNumber();
+    Console.WriteLine(name);
+    Console.WriteLine(number);
+
+       
     }
+
+
+
+
 }
