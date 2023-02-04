13fa27c1450d43933c52b182f2d909d70e10b3fb
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-04 11:34:46 +0200

    Made waiting animation smoother

--- prove/Develop04/Animation.cs
+++ prove/Develop04/Animation.cs
@@ -6,18 +6,20 @@ public class Animation
 
     public void Display(int duration)
     {
-        int cycles = duration * 5 / 6;
+        int cycles = duration;
         
         Console.Write(" ");
 
         for (int i = 0; i < cycles; i++)
         {
             Console.Write("\b \b-");
-            Thread.Sleep(400);
+            Thread.Sleep(300);
             Console.Write("\b \b\\");
-            Thread.Sleep(400);
+            Thread.Sleep(200);
+            Console.Write("\b \b|");
+            Thread.Sleep(200);
             Console.Write("\b \b/");
-            Thread.Sleep(400);
+            Thread.Sleep(300);
         }
 
         Console.Write("\b \b\n");
