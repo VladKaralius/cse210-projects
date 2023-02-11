7fe3761cfe19821ac95c797f5e191f5302e0a860
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-11 07:58:17 +0200

    Menu structure complete

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        
+        int choice = 0;
+        while (choice != 7)
+        {
+            Console.WriteLine("\nYou have 0 points.\n");
+            Console.WriteLine("Menu Options:\n" +
+                              "  1. Create New Goal\n" +
+                              "  2. List Goals\n" +
+                              "  3. Save Goals\n" +
+                              "  4. Load Goals\n" +
+                              "  5. Record Event\n" +
+                              "  6. Gamify (add later)\n" +
+                              "  7. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine(choice);
+            }
+
+            else if (choice == 2)
+            {
+                Console.WriteLine(choice);
+            }
+
+            else if (choice == 3)
+            {
+                Console.WriteLine(choice);
+            }
+
+            else if (choice == 4)
+            {
+                Console.WriteLine(choice);
+            }
+
+            else if (choice == 5)
+            {
+                Console.WriteLine(choice);
+            }
+
+            else if (choice == 6)
+            {
+                Console.WriteLine(choice);
+            }
+        } 
     }
 }
