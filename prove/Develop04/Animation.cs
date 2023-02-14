99e4dec8a9dd72fb164c7e43d39337e4eeb01fe3
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-14 14:34:33 +0200

    Improved symbol cycling

--- prove/Develop04/Animation.cs
+++ prove/Develop04/Animation.cs
@@ -8,18 +8,17 @@ public class Animation
     {
         int cycles = duration;
         
+        List<string> symbols = new List<string>{"-", "\\", "|", "/"};
+
         Console.Write(" ");
 
         for (int i = 0; i < cycles; i++)
         {
-            Console.Write("\b \b-");
-            Thread.Sleep(250);
-            Console.Write("\b \b\\");
-            Thread.Sleep(250);
-            Console.Write("\b \b|");
-            Thread.Sleep(250);
-            Console.Write("\b \b/");
-            Thread.Sleep(250);
+            for (int j = 0; j < symbols.Count(); j++)
+            {
+                Console.Write($"\b \b{symbols[j]}");
+                Thread.Sleep(250);
+            }
         }
 
         Console.Write("\b \b\n");
