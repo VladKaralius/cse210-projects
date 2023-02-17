0d4fe66098248395e5aaa39d877162f2d3a06f43
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-17 15:57:56 +0200

    Created Level class

---
+++ prove/Develop05/Level.cs
@@ -0,0 +1,14 @@
+public class Level
+{
+    private int _increase = 100;
+
+    Level()
+    {
+
+    }
+
+    public void Display(int pointTotal, int pointsGained)
+    {
+        int level = pointTotal / 1;
+    }
+}
