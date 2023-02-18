e23712d04d515037d3dcdb4e931c9c358aaa7400
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-18 17:52:01 +0200

    Added basic menus

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        int choice = 0;
+        while (choice != 6)
+        {
+
+            Console.WriteLine("Menu Options:\n" +
+                              "  1. Create New Goal\n" +
+                              "  2. List Goals\n" +
+                              "  3. Save Goals\n" +
+                              "  4. Load Goals\n" +
+                              "  5. Record Event\n" +
+                              "  6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+
+            }
+
+            else if (choice == 2)
+            {
+
+            }
+
+            else if (choice == 3)
+            {
+   
+            }
+
+            else if (choice == 4)
+            {
+
+            }
+
+            else if (choice == 5)
+            {
+
+            }
+        }
     }
 }
