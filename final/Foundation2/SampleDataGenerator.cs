97f007dd8d9f7aafb435e11f044d22141cab185f
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-22 10:14:23 +0200

    SampleDataGenerator completed

--- final/Foundation2/SampleDataGenerator.cs
+++ final/Foundation2/SampleDataGenerator.cs
@@ -1,20 +1,13 @@
 public class SampleDataGenerator
 {
-    private string[] _titles =
+    private string[] _productNames =
         {
-            "12 Steps To Finding The Perfect Cat",
-            "Cats Are Taking Over And What To Do About It",
-            "5 Lessons About Cats You Can Learn From Superheroes",
-            "Top Reasons People Hate Dogs",
-            "What Hollywood Can Teach Us About Dogs",
-            "Become An Expert On Dogs By Watching This Video",
-            "9 Signs You're A Hamster Expert",
-            "The History Of Hamsters Told Through Tweets",
-            "This Week's Top Stories About Hamsters",
-            "The Rabbits You'll Never Forget"
+            "Algecir", "Hosphere", "Theidonial", "Brochets", "Examplier",
+            "Erneather", "Crossibilia", "Orangence", "Germantina", "Fundards",
+            "Lupervices", "Bulgence", "Haemation", "Paludlow", "Apparabs"
         };
     
-    private int _currentTitlesIndex = 0;
+    private int _currentProductNamesIndex = 0;
 
     private string[] _names = 
         {
@@ -27,43 +20,57 @@ public class SampleDataGenerator
     
     private int _currentNamesIndex = 0;
     
-    private string[] _texts =
+    private string[] _countries =
         {
-            "You are remarkably ignorant about this subject. But still you post.",
-            "This is understandable, laudable and just downright adorable.",
-            "I'm having much trouble working out what you're saying.",
-            "I think given the observations so far it is unnecessary to post a rejoinder.",
-            "Let me put my vote down against this woefully bloated tomfoolery. I am sick to death of this.",
-            "It's beyond comical, enough to make a meerkat do continuous hysterical somersaults for an eternity.",
-            "When will folks stop orchestrating a maelstrom of misinformation?",
-            "First things first. Now let the chorus of loudmouths sing forth.",
-            "Well, yes and no. But mostly you must be joking. Probably it'll wither on the vine, but I hope not.",
-            "A witless response. It's everyone's fault, we all share the blame and we shall all suffer the consequences.",
-            "Perceptive talk. Sometimes pointing out the simple seems to be a revelation...",
-            "It's hard to work out what still seems staggeringly amazing.",
-            "I seriously doubt that happened. We have had enough of that sort in the bygone decade!!!",
-            "Deeply categorically untrue. In that situation the loneliness must be overwhelming.",
-            "Typical. Absolute hypocrisy!!!!!!",
-            "A pretty darned fair point, and one that I support as well.",
-            "Must be irony. No sane person could truly think so irrationally.",
-            "Sad but true. I don't believe you, I think you are being inane.",
-            "This piece is the best proof yet that Animal Farm remains as true today as ever.",
-            "I've just had a flash of inspiration. Said it before and I'll say it again!"
+            "Resoland", "Glacolia", "Republic of Eucon", "Livonshire Islands", "Agariastan", "Itau",
+            "Genovia", "Buranda", "Drenai Empire", "Freedonia", "Laputa", "San Marcos"
         };
     
-    private int _currentTextsIndex = 0;
+    private int _currentCountriesIndex = 0;
+
+    private string[] _states =
+        {
+            "Alderney", "Calisoga", "Catawaba", "Coventry", "Delmarva", "Grojaria", 
+            "Florin", "Gilead", "Ganbe Nezea", "Etotantoro", "Guacarba", "Rane"
+
+        };
+    
+    private int _currentStatesIndex = 0;
+    
+    private string[] _statesUSA =
+        {
+            "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+            "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+            "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+            "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
+            "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+            "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+            "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+            "West Virginia", "Wisconsin", "Wyoming"
+        };
+    
+    private int _currentStatesUSAIndex = 0;
+
+    private string[] _streets =
+        {
+            "Luna Street", "Lilypad Route", "Shade Lane", "Crimson Street",
+            "Mason Passage", "Genral Boulevard", "Liberty Row", "Garden Avenue",
+            "Congress Street", "College Way", "Duke Street", "Legend Street"
+        };
+    
+    private int _currentStreetsIndex = 0;
 
     public SampleDataGenerator()
     {
         Random random = new Random();
         
-        // Shuffle _titles
-        Double[] order = new Double[_titles.Length];
-        for (int i = 0; i < _titles.Length; i++)
+        // Shuffle _productNames
+        Double[] order = new Double[_productNames.Length];
+        for (int i = 0; i < _productNames.Length; i++)
         {
             order[i] = random.NextDouble();
         }
-        Array.Sort(order, _titles);
+        Array.Sort(order, _productNames);
 
         // Shuffle _names
         order = new Double[_names.Length];
@@ -73,20 +80,44 @@ public class SampleDataGenerator
         }
         Array.Sort(order, _names);
 
-        // Shuffle _texts
-        order = new Double[_texts.Length];
-        for (int i = 0; i < _texts.Length; i++)
+        // Shuffle _countries
+        order = new Double[_countries.Length];
+        for (int i = 0; i < _countries.Length; i++)
         {
             order[i] = random.NextDouble();
         }
-        Array.Sort(order, _texts);
+        Array.Sort(order, _countries);
+
+        // Shuffle _states
+        order = new Double[_states.Length];
+        for (int i = 0; i < _states.Length; i++)
+        {
+            order[i] = random.NextDouble();
+        }
+        Array.Sort(order, _states);
+
+        // Shuffle _statesUSA
+        order = new Double[_statesUSA.Length];
+        for (int i = 0; i < _statesUSA.Length; i++)
+        {
+            order[i] = random.NextDouble();
+        }
+        Array.Sort(order, _statesUSA);
+
+        // Shuffle _streets
+        order = new Double[_streets.Length];
+        for (int i = 0; i < _streets.Length; i++)
+        {
+            order[i] = random.NextDouble();
+        }
+        Array.Sort(order, _streets);
     }
 
-    public string GetNextTitle()
+    public string GetNextProductName()
     {
-        string title = _titles[_currentTitlesIndex];
-        _currentTitlesIndex = ++_currentTitlesIndex % _titles.Length;
-        return title;
+        string productName = _productNames[_currentProductNamesIndex];
+        _currentProductNamesIndex = ++_currentProductNamesIndex % _productNames.Length;
+        return productName;
     }
 
     public string GetNextName()
@@ -96,11 +127,33 @@ public class SampleDataGenerator
         return name;
     }
 
-    public string GetNextText()
+    public string GetNextCountry()
     {
-        string text = _texts[_currentTextsIndex];
-        _currentTextsIndex = ++_currentTextsIndex % _texts.Length;
-        return text;
+        string country = _countries[_currentCountriesIndex];
+        _currentCountriesIndex = ++_currentCountriesIndex % _countries.Length;
+        return country;
     }
+
+    public string GetNextState()
+    {
+        string state = _states[_currentStatesIndex];
+        _currentStatesIndex = ++_currentStatesIndex % _states.Length;
+        return state;
+    }
+
+    public string GetNextStateUSA()
+    {
+        string stateUSA = _statesUSA[_currentStatesUSAIndex];
+        _currentStatesUSAIndex = ++_currentStatesUSAIndex % _statesUSA.Length;
+        return stateUSA;
+    }
+    
+    public string GetNextStreet()
+    {
+        string street = _streets[_currentStreetsIndex];
+        _currentStreetsIndex = ++_currentStreetsIndex % _streets.Length;
+        return street;
+    }
+    
 
 }
