e129b2fb324e961e57bde3172e84640f93429b89
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-24 15:22:30 +0200

    Event class draft created

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,36 @@
+public class Event
+{
+    protected string _title;
+    protected string _description;
+    protected string _date;
+    protected string _time;
+    private Address _address;
+
+    Event(string title, string description, string date, string time)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+    }
+
+    public void SetAddress(string streetAddress,
+        string city, string state, string country)
+    {
+        _address= new Address(streetAddress, city, state, country);
+    }
+
+    public string GetStandardDetails()
+    {
+        string standardDetails = $"Invitation to the event: {_title}\n" +
+            $"{_description}\n{_date} {_time}\n{_address.GetFullAddress()}";
+        
+        return standardDetails;
+    }
+
+    public string GetShortDescription()
+    {
+        string type = GetType().Name;
+        return $"{type}{_title} held on {_date}";
+    }
+}
