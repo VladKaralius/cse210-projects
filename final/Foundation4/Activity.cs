753a3f4fe290ea2ceebe1372991d598ffce3920c
VladKaralius <vladislovas.karalius@gmail.com>
2023-02-25 19:36:40 +0200

    Created Activity class

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,7 +1,23 @@
-public class Activity
+public abstract class Activity
 {
-    public Activity()
+    private string _date;
+    private int _length;
+
+    public Activity(string date, int length)
     {
-        
+        _date = date;
+        _length = length;
+    }
+
+    public abstract float GetDistance();
+    public abstract float GetSpeed();
+    public abstract float GetPace();
+
+    public string GetSummary()
+    {
+        return  $"{_date} TYPE ({_length} min): " +
+                $"Distance: {GetDistance()} km, " +
+                $"Speed: {GetSpeed()} kph, "+
+                $"Pace: {GetPace()} min per km";
     }
 }
